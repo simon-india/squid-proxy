badf7200d5bf2bf253ca75b43bf479bd76c182b0
simonindia <23007053+simon-india@users.noreply.github.com>
2021-03-03 15:13:54 +0530

    Update entry.sh
    Some messed up password generation

--- entry.sh
+++ entry.sh
@@ -7,11 +7,24 @@ if [ -z "${USERNAME}" ]; then
   echo "You have to specify the -e USERNAME=... argument"
   exit
 fi
-if [ -z "${PASSWORD}" ]; then
-  echo "You have to specify the -e PASSWORD=... argument"
-  exit
-fi
+#if [ -z "${PASSWORD}" ]; then
+#  echo "You have to specify the -e PASSWORD=... argument"
+#  exit
+#fi
+PASSWORD=$(openssl rand -base64 12)
 
 htpasswd -cb /etc/squid/passwd "${USERNAME}" "${PASSWORD}"
 
 exec $(which squid) -NYCd 1
+
+echo "+++++++++++++++++++++++++++++++++++"
+
+echo $PASSWORD
+
+echo "+++++++++++++++++++++++++++++++++++"
+
+echo "+++++++++++++++++++++++++++++++++++"
+
+echo $PASSWORD
+
+echo "+++++++++++++++++++++++++++++++++++"
