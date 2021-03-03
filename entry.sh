290013a08f45239ea2f19fa691ce2a26636b10e4
simonindia <23007053+simon-india@users.noreply.github.com>
2021-03-03 15:27:58 +0530

    Update entry.sh

--- entry.sh
+++ entry.sh
@@ -3,14 +3,15 @@
 set -x
 set -e
 
-if [ -z "${USERNAME}" ]; then
-  echo "You have to specify the -e USERNAME=... argument"
-  exit
-fi
+#if [ -z "${USERNAME}" ]; then
+#  echo "You have to specify the -e USERNAME=... argument"
+#  exit
+#fi
 #if [ -z "${PASSWORD}" ]; then
 #  echo "You have to specify the -e PASSWORD=... argument"
 #  exit
 #fi
+USERNAME=squid
 PASSWORD=$(openssl rand -base64 12)
 
 htpasswd -cb /etc/squid/passwd "${USERNAME}" "${PASSWORD}"
@@ -19,11 +20,11 @@ exec $(which squid) -NYCd 1
 
 echo "+++++++++++++++++++++++++++++++++++"
 
-echo $PASSWORD
+echo $USERNAME
 
 echo "+++++++++++++++++++++++++++++++++++"
 
-echo "+++++++++++++++++++++++++++++++++++"
+echo "/\/\/\/\/\/\\/\/\/\/\/\\/\/\/\\/\/\/\/\/\/\"
 
 echo $PASSWORD
 
